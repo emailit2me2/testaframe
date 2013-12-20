83285c0301cba6d9c4f12951ea35827ab4aa596e
Mark Garboden <MarkGarboden@gmail.com>
2013-12-19 19:01:07 -0800

    Added basic working ruby implementation.

--- ruby/base_page.rb
+++ ruby/base_page.rb
@@ -10,20 +10,16 @@ class PageFactory
     @driver = driver
     @base_url = base_url
   end
-  def at(page_class)
-    url_params=""
-    url = "#{@base_url}#{page_class::PAGE}#{url_params ? '?' : ''}#{url_params}"
-    new_page = page_class.new(self)
-    @driver.get(url)
+  def at(page_class, params='', substitutions='')
+    new_page = page_class.new(self, params, substitutions)
+    @driver.get(new_page.full_url)
     #@log.debug("Goto page #{url}")
     new_page.verify_on_page()
     return new_page
   end
-  def on_page(page_class)
-    url_params=""
-    url = "#{@base_url}#{page_class::PAGE}#{url_params ? '?' : ''}#{url_params}"
-    new_page = page_class.new(self)
-    @driver.get(url)
+  def on_page(page_class, params='', substitutions='')
+    new_page = page_class.new(self, params, substitutions)
+    @driver.get(new_page.full_url)
     #@log.debug("On page #{url}")
     new_page.verify_on_page()
     return new_page
@@ -37,10 +33,31 @@ class ObsoletePage
 end
 
 class BasePageObj
-  def initialize(factory)
+  TIMEOUT = 10.0
+  DELAY = 1.0
+  def initialize(factory, params, substitutions)
     @factory = factory
     @driver = @factory.driver
+    @params = params
+    @substitutions = substitutions
+    if not self.class::PAGE_SUB.nil?
+      @page = self.class::PAGE_SUB % {:optional_l=>'l'}
+    elsif self.respond_to? :PAGE
+      @page = self.class:PAGE
+    else
+      raise NotImplementedError, 'PAGE or PAGE_SUB must be defined per page class'
+    end
+    if self.respond_to? :PAGE_RE
+      @page_re = self.class::PAGE_RE
+    else
+      @page_re = Regexp.new('^' + @page + '$')
+    end
     prep_finders()
+  end
+  def full_url
+    base_url = @factory.base_url   TODO FIXME
+    url = "#{base_url}#{@page}#{@params}"
+    return url
   end
 
   def now_on(page_class)
@@ -50,7 +67,7 @@ class BasePageObj
   end
   def verify_on_page
     path = URI.parse(@driver.current_url).path
-    fail "Path to page expected\n#{self.class::PAGE} did not match \n#{path}\n" unless self.class::PAGE == path
+    fail "Path to page expected\n#{@page} did not match \n#{path}\n" unless path =~ @page_re
     #p @verify_element
     unless @verify_element
       raise NotImplementedError, "All page objects must have a @verify_element"
@@ -59,7 +76,33 @@ class BasePageObj
   end
 
   def find_the(element_spec)
-    return @driver.find_element element_spec.how => element_spec.spec
+    start = Time.now
+    while Time.now - start < TIMEOUT
+      begin
+        puts "find_the #{element_spec}"
+        return @driver.find_element element_spec.how => element_spec.spec
+      rescue => err
+        puts "Delay find_the: #{element_spec} - #{err}"
+        #puts err
+        sleep(DELAY)
+      end
+    end
+    raise err
+  end
+
+  def find_all(element_spec)
+    start = Time.now
+    while Time.now - start < TIMEOUT
+      begin
+        puts "find_the #{element_spec}"
+        return @driver.find_elements element_spec.how => element_spec.spec
+      rescue => err
+        puts "Delay find_the: #{element_spec} - #{err}"
+        #puts err
+        sleep(DELAY)
+      end
+    end
+    raise err
   end
 
   def click_on(element_spec)
@@ -76,21 +119,39 @@ class BasePageObj
   end
 
   def method_missing(m, *args, &block)
+    p "lookup #{m}"
     sym = ('@'+m.to_s).to_sym
     if instance_variables.include? sym
-      return instance_variable_get sym
+      ret = instance_variable_get sym
+      ret.name = m.to_s
+      return ret
     else
       super
     end
   end
-  def by_name(locator)
-    return ByBase.new(self, :name, locator)
-  end
   def by_css(locator)
     return ByBase.new(self, :css, locator)
   end
+  def by_id(locator)
+    return ByBase.new(self, :id, locator)
+  end
+  def by_name(locator)
+    return ByBase.new(self, :name, locator)
+  end
+  def by_tag_name(locator)
+    return ByBase.new(self, :tag_name, locator)
+  end
+  def by_class_name(locator)
+    return ByBase.new(self, :class_name, locator)
+  end
   def by_link_text(locator)
     return ByBase.new(self, :link_text, locator)
+  end
+  def by_partial_link_text(locator)
+    return ByBase.new(self, :partial_link_text, locator)
+  end
+  def by_xpath(locator)
+    return ByBase.new(self, :xpath, locator)
   end
 end
 
@@ -101,9 +162,34 @@ class ByBase
     @how = how
     @spec = spec
   end
+  def name=(my_name)
+    @name = my_name
+  end
+  def to_s
+    return @name
+  end
 
   def the_text
-    return @page.find_the(self).text
+    @page.find_the(self).text
+    #proc {@page.find_the(self).text }
+  end
+  def all_the_text
+    @page.find_all(self).map {|e| e.text}
+  end
+  def the_attribute(attrib)
+    @page.find_the(self).attribute(attrib)
+  end
+  def all_the_attributes(attrib)
+    @page.find_all(self).map {|e| e.attribute(attrib)}
+  end
+  def is_this_enabled
+    @page.find_the(self).enabled?
+  end
+  def is_this_displayed
+    @page.find_the(self).displayed?
+  end
+  def length_of
+    @page.find_all(self).length
   end
 
 end
--- ruby/gui_test.rb
+++ ruby/gui_test.rb
@@ -7,21 +7,40 @@ gem 'test-unit'
 require 'base_tst'
 require 'gui_pages'
 
-
 class TestMyGui < TestCaseBase
   def test_ajaxy
     ajaxy_page = @start.at(AjaxyPage)
     new_label1 = 'new_label1'
     ajaxy_page.fillout_form(new_label1)
     ajaxy_page.submit_fillout_form()
-    sleep(8.0)
-    try_is_equal(new_label1, ajaxy_page.new_labels.the_text)
+    #sleep(8.0)
+    try_is_equal('label') {ajaxy_page.new_labels.the_attribute('class')} # lame example
+    try_is_equal(['label'], lambda {ajaxy_page.new_labels.all_the_attributes('class')}) # lame example
+    try_is_equal(new_label1,new_label1)
+    try_is_equal(new_label1,ajaxy_page.new_labels.the_text)
   end
   def test_next_page
     linked_page = @start.at(LinkedImagePage)
     next_page = linked_page.goto_next_page()
     assert_raise NotImplementedError do
+      # because using obsolete page
       linked_page.next_page_link.the_text
     end
+  end
+  def test_form_page
+    # TODO was hoping to get a param in the URL
+    form_page = @start.at(FormPage)
+    try_is_equal(true, form_page.check_enabled.is_this_enabled)
+    try_is_equal(false, form_page.check_disabled.is_this_enabled)
+    try_is_equal(true, form_page.check_enabled.is_this_displayed)
+
+    #sleep(2.0)
+    next_page = form_page.do_submit()
+    try_is_equal(2, next_page.list_items.length_of)
+    try_is_equal(['Item 1','Item 2'], next_page.list_items.all_the_text)
+  end
+  def test_hidden
+    hidden_page = @start.at(HiddenPage, {:optional_l=>'l'})
+    try_is_equal(false, hidden_page.verify_element.is_this_displayed)
   end
 end
